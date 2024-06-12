06b93e8a017b85985c9e9f8eb16254911a88f8ea
Nils Cremer <nils.crmr@gmail.com>
2024-06-12 18:13:17 +0200

    Large multiplication

--- tests/test_numbers.py
+++ tests/test_numbers.py
@@ -1,5 +1,5 @@
 from colored_networks.lambda_calc import App
-from colored_networks.lambda_constants import add, mul, zero, one
+from colored_networks.lambda_constants import add, mul, zero, one, num
 
 
 def test_mul_1_1():
@@ -14,3 +14,10 @@ def test_add_0_1():
     net = add_1_2.to_colored_network()
     net.reduce()
     assert net.seems_isomorphic_to(one.to_colored_network())
+
+
+def mul_5_7():
+    mul_5_7 = App(App(mul, num(5)), num(7))
+    net = mul_5_7.to_colored_network()
+    net.reduce()
+    assert net.seems_isomorphic_to(num(35).to_colored_network())
